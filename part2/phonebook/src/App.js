a752e2fb677da0f3597ab9b1c41a494e53aeda8d
ansakatu <annika.sk.tuulivuori@student.jyu.fi>
2023-03-24 16:42:02 +0200

    2.9*: phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const checkPerson = (event) => {
     event.preventDefault()
@@ -32,7 +39,7 @@ const App = () => {
     window.alert(`${newName} is already added to phonebook`)
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
@@ -42,31 +49,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={checkPerson}>
-        <div>
-          name: <input 
-            value={newName}
-            onChange={handlePersonChange}/>
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={newFilter} onChange={handleFilterChange} />
+      <h2>add a new</h2>
+      <PersonForm 
+        onSubmit={checkPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
-      </ul>
-      <div>debug: {newName}</div>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
     
   )
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Person from "./Person"
+
+const Persons = ({ persons, newFilter }) => {
+    const personsToShow = persons.filter((person) =>
+        person.name.toLowerCase().includes(newFilter.toLowerCase())
+    )
+    return (
+        <div>
+          {personsToShow.map((person) => 
+            <Person key={person.name} person={person}/>
+          )}
+        </div>
+    )
+}
+
+export default Persons
