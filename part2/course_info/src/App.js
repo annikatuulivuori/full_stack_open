313f5e56e93c9d560b14f732c19884c9f0c06ebe
ansakatu <annika.sk.tuulivuori@student.jyu.fi>
2023-03-24 13:18:04 +0200

    2.1: step6

--- part2/course_info/src/App.js
+++ part2/course_info/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Course = ({ course }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course}/>
+      <Content parts={course.parts}/>
     </div>
-  );
+  )
 }
 
-export default App;
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>
+        {course.name}
+      </h1>
+      <div>
+        {course.course}
+      </div>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part}/>
+      )}
+    </div>
+  )
+}
+
+/*const Total = ( props ) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    </div>
+  )
+}*/
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
