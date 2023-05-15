eb1e4292a1ed58aea406de3f80d80deec6b53cc6
ansakatu <annika.sk.tuulivuori@student.jyu.fi>
2023-05-15 16:41:43 +0300

    2.16: phonebook step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState()
 
   useEffect(() => {
     personService
@@ -40,6 +42,10 @@ const App = () => {
             .then(returnedPerson => {
               setPersons(persons.map(p => p.id === returnedPerson.id ? returnedPerson : p))
               clearInputs()
+              setErrorMessage(`Person's ${previousPerson.name} number changed`)
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 5000)
             })
             .catch(error => console.log("Error in updating person"))
         }
@@ -62,6 +68,10 @@ const App = () => {
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
         clearInputs()
+        setErrorMessage(`Person ${personObject.name} added`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
       .catch(error => console.log("Error in adding person"))
   }
@@ -77,6 +87,10 @@ const App = () => {
           const updatedPersons = persons.filter(person => person.id !== id)
           setPersons(updatedPersons)
           clearInputs()
+          setErrorMessage(`Person ${person.name} deleted`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
         })
         .catch(error => console.log("Error in removing person"))
     }
@@ -104,6 +118,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter value={newFilter} onChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm 
