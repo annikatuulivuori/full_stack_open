15dc314454faf3fda463e650e64158dd08285a2d
annika <atuulivuo@gmail.com>
2023-07-15 14:06:07 +0300

    4: render fix

--- part4/index.js
+++ part4/index.js
@@ -1,32 +1,23 @@
-const http = require('http')
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const url = `mongodb+srv://annika:<password>@cluster0.ypixfhw.mongodb.net/blogApp?retryWrites=true&w=majority`
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
 app.use(cors())
 app.use(express.json())
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog =  mongoose.model('Blog', blogSchema)
+
+const Blog =  require('./models/blog')
 
 app.get('/api/blogs', (request, response) => {
 	Blog.find({}).then(blog => {
 		response.json(blog)
-    mongoose.connection.close()
 	})
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const body = new Blog(request.body)
 
   if (body.title === undefined) {
@@ -51,9 +42,24 @@ app.post('/api/blogs', (request, response) => {
     .then(savedBlog => {
       response.json(savedBlog)
     })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT ||3003
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
