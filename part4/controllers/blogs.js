383df1d28efaf4465697afdca39547fa1ca72ff2
annika <atuulivuo@gmail.com>
2023-07-20 19:02:14 +0300

    4.18-4.20*: bloglist expansions step6-8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -15,40 +16,41 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body
+  const body = request.body
 
-    const user = await User.findById(body.userId)
-
-    // console.log('Request Body:', body)
-    // console.log('User:', user)
-
-    if (body.title === undefined) {
-      return response.status(400).json({ error: 'title missing' })
-    }
-    if (body.author === undefined) {
-      return response.status(400).json({ error: 'author missing' })
-    }
-    if (body.url === undefined) {
-      return response.status(400).json({ error: 'url missing' })
-    }
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes || 0,
-      user: user._id
-    })
-
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    console.log(error)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+
+  const user = await User.findById(decodedToken.id)
+
+  // console.log('Request Body:', body)
+  // console.log('User:', user)
+
+  if (body.title === undefined) {
+    return response.status(400).json({ error: 'title missing' })
+  }
+  if (body.author === undefined) {
+    return response.status(400).json({ error: 'author missing' })
+  }
+  if (body.url === undefined) {
+    return response.status(400).json({ error: 'url missing' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
