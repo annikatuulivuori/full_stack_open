3dee246cfb9e19236cc5646e44c5ab7d6c850e0f
annika <atuulivuo@gmail.com>
2023-07-20 13:48:30 +0300

    4.17: bloglist extensions step5

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
-const Blog =  require('../models/blog')
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -14,27 +15,45 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const body = new Blog(request.body)
+  try {
+    const body = request.body
 
-  if (body.title === undefined) {
-    return response.status(400).json({ error: 'title missing' })
-  }
-  if (body.author === undefined) {
-    return response.status(400).json({ error: 'author missing' })
-  }
-  if (body.url === undefined) {
-    return response.status(400).json({ error: 'url missing' })
-  }
+    const user = await User.findById(body.userId)
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-  })
+    // console.log('Request Body:', body)
+    // console.log('User:', user)
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+    if (body.title === undefined) {
+      return response.status(400).json({ error: 'title missing' })
+    }
+    if (body.author === undefined) {
+      return response.status(400).json({ error: 'author missing' })
+    }
+    if (body.url === undefined) {
+      return response.status(400).json({ error: 'url missing' })
+    }
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
@@ -52,11 +71,6 @@ blogsRouter.put('/:id', async (request, response) => {
   } else {
     response.status(404).end()
   }
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
 })
 
 module.exports = blogsRouter
