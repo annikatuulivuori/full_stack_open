0d16e25c02addf8bcfcfe662dcc203e2eb94e4ab
annika <atuulivuo@gmail.com>
2023-07-20 21:20:10 +0300

    4.22*: bloglist expansions step10

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
+const middleware = require('../utils/middleware')
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -17,13 +16,7 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   // console.log('Request Body:', body)
   // console.log('User:', user)
@@ -53,21 +46,16 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const user = request.user
 
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const userId = decodedToken.id
   const blog = await Blog.findById(request.params.id)
 
   if (!blog) {
     return response.status(404).json({ error: 'blog not found' })
   }
 
-  if (blog.user.toString() !== userId.toString()) {
+  if (blog.user.toString() !== user._id.toString()) {
     return response.status(403).json({ error: 'no authorization to delete this blog' })
   }
 
