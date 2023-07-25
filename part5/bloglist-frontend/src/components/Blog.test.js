ac8dfc40373863684876c1059ba60a8dfed0460c
annika <atuulivuo@gmail.com>
2023-07-25 17:07:00 +0300

    5.14: bloglist tests step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title by default', () => {
   const blog = {
-    id: '123',
+    id: '1234',
     title: 'Test Blog',
     author: 'Test Author',
-    url: 'http://test-url.com',
-    likes: 5,
+    url: 'http://test.com',
+    likes: 2,
     user: {
       username: 'testuser',
     },
@@ -19,4 +20,31 @@ test('renders blog title by default', () => {
 
   const titleElement = screen.getByText(/Test Blog by Test Author/i)
   expect(titleElement).toBeDefined()
+})
+
+test('clicking "view" button calls event handler once', async () => {
+  const blog = {
+    id: '1234',
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://test.com',
+    likes: 2,
+    user: {
+      username: 'testuser',
+    },
+  }
+
+  render(<Blog blog={blog} user={null} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const url = screen.getByText('URL: http://test.com')
+
+  const likes = screen.getByText('Likes: 2')
+
+  expect(url).toBeInTheDocument()
+  expect(likes).toBeInTheDocument()
+
 })
